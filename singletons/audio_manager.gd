d6751054aad4ca4abede364f105987746a18d102
Kallroh <none@none.none>
2025-04-13 16:50:17 +0200

    add music

--- singletons/audio_manager.gd
+++ singletons/audio_manager.gd
@@ -1,9 +1,22 @@
 extends Node
 class_name AudioManagerScript
 
+var current_music_player : AudioStreamPlayer
+
 func play_sound(name: String) -> void:
 	var audio_player = get_node(name)
 	if not audio_player:
 		push_warning("Unknowned sound : %s" % name)
 		return
 	audio_player.play()
+
+func play_music(name: String) -> void:
+	var audio_player = get_node(name)
+	if not audio_player:
+		push_warning("Unknown sound : %s" % name)
+		return
+	if audio_player == current_music_player: return
+	else:
+		current_music_player.stop()
+		audio_player.play()
+		current_music_player = audio_player
